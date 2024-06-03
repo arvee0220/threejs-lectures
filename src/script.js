3fd88e5b108d97013414c1aa7e7f6ea5110b6334
arvee0220 <rowellverdejo@gmail.com>
2024-06-03 11:40:06 -0400

    commit updates

--- src/script.js
+++ src/script.js
@@ -1,30 +1,56 @@
-import * as THREE from 'three';
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 // initialize the scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 // add objects to the scene
-const cubeGeometry = new THREE.BoxGeometry(1,1,1)
-const cubeMaterial = new THREE.MeshBasicMaterial({color: "red"})
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+const cubeMaterial = new THREE.MeshBasicMaterial({ color: "red" });
 
-const cubeMesh = new THREE.Mesh(
-  cubeGeometry,
-  cubeMaterial
-)
-scene.add(cubeMesh)
+const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
+scene.add(cubeMesh);
 
 // initialize the camera
-const camera = new THREE.PerspectiveCamera(
-  75, 
-  window.innerWidth / window.innerHeight,
-  0.1,
-  30)
-camera.position.z = 5
+/* const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 35);
+ */
+
+const aspectRatio = window.innerWidth / window.innerHeight;
+
+const camera = new THREE.OrthographicCamera(-1 * aspectRatio, 1 * aspectRatio, 1, -1, 0.1, 200);
+camera.position.z = 5;
 
 // initialize the renderer
-const canvas = document.querySelector('canvas.threejs')
+const canvas = document.querySelector("canvas.threejs");
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas
-})
-renderer.setSize(window.innerWidth, window.innerHeight)
-renderer.render(scene, camera)
+	antialias: true,
+	canvas: canvas,
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
+const maxPixelRation = Math.min(window.devicePixelRatio, 2);
+
+// Anti-aliasing solution
+renderer.setPixelRatio(window.devicePixelRatio);
+console.log(window.devicePixelRatio);
+
+// instantiate controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+controls.autoRotate = true;
+
+window.addEventListener("resize", () => {
+	camera.aspect = aspectRatio;
+	camera.updateProjectionMatrix();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+// render the scene
+const renderloop = () => {
+	controls.update();
+
+	renderer.render(scene, camera);
+
+	window.requestAnimationFrame(renderloop);
+};
+
+renderloop();
